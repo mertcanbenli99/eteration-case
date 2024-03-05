12a0cd39d35a7aec98545702587cac8b92ceeace
Mert Can Benli <mertcanbenli99@outlook.com>
2024-03-05 16:37:19 +0300

    mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MovieController } from './movie/movie.controller';
 import { MovieService } from './movie/movie.service';
 import { ConfigModule } from '@nestjs/config';
-import { HttpModule, HttpService } from '@nestjs/axios';
-
+import { HttpModule} from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule],
+  imports: [ConfigModule.forRoot(),
+     HttpModule, MongooseModule.forRoot('mongodb://localhost:27017/eteration-case')],
   controllers: [MovieController],
   providers: [MovieService],
 })
