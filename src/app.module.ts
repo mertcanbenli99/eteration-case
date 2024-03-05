36bfb595022a63361dcf3cb7fcda3cbcb7b834d2
Mert Can Benli <mertcanbenli99@outlook.com>
2024-03-05 12:58:16 +0300

    project initialization

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MovieController } from './movie/movie.controller';
+import { MovieService } from './movie/movie.service';
+
+
+@Module({
+  imports: [],
+  controllers: [MovieController],
+  providers: [MovieService],
+})
+export class AppModule {}
